638cfc9e9362cf4da0f7530c5da702c3a2a156d7
Cyril MORISSE <cmorisse@boxes3.net>
2021-10-20 09:20:58 +0200

    IMP: logging

--- config.py
+++ config.py
@@ -55,8 +55,8 @@ def load_env_vars():
             'INOUK_SESSION_STORE_DBTABLE': INOUK_SESSION_STORE_DBTABLE,
             'INOUK_SESSION_STORE_DEBUG': INOUK_SESSION_STORE_DEBUG
         }
-        _logger.info("inouk_session_store ENV VARs loaded?")
-    _logger.info("inouk_session_store active ENV VARs: %s", ENV_VARS)
+        _logger.info("inouk_session_store params loaded from ENV VARs.")
+    _logger.info("inouk_session_store params: %s", ENV_VARS)
     return ENV_VARS
 
 if ENV_VARS is None:
